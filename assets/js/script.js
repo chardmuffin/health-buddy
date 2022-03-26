b0574a202c6c442e75dffa395c1f38578be17ae9
Richard Huffman <richardhuffman96@gmail.com>
2022-03-26 12:49:55 -0400

    added a hamburger menu

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+// TODO:
+// this function is called when the "settings" menu item is tapped
+var showSettings = function() {
+
+}
+
+// TODO:
+// this function is called when the "about" menu item is tapped
+var showAbout = function() {
+
+}
+
+// Listeners
+document.getElementById("settings").addEventListener("click", showSettings);
+document.getElementById("about").addEventListener("click", showAbout);
